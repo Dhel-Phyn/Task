4b3413fd35c86ff88b6053f426f856e4b04c0a21
Dhel-Phyn <dhelphyn@gmail.com>
2022-06-29 00:15:03 +0000

    Dashboard Face One

---
+++ index.js
@@ -0,0 +1,10 @@
+function validate(){
+  if (document.myform.Email.value = ""){
+    alert("Please provide a valid email ID")
+    document.myform.Email.focus()
+    return false
+  }
+  else if(document.myform.Password.value == ""){
+    alert((((((((((("zzzzzzzzzzzzzzzzzz")))))))))))
+  }
+}
